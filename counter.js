aa5da0d4bfbbd6dc83e65c84ebee001cdfcf9437
ayaka-windows <ayakaka0814@gmail.com>
2025-06-20 20:23:13 +0900

    Added buttons and alert

--- counter.js
+++ counter.js
@@ -6,13 +6,24 @@
     
         if($targetButton.textContent === "+"){
             $counter.textContent = currentCount + 1;  
+        }else if($targetButton.textContent === "+10"){
+            $counter.textContent = currentCount + 10; 
+        }else if($targetButton.textContent === "-10"){
+            $counter.textContent = currentCount - 10; s
         }else{
             $counter.textContent = currentCount - 1;
+        }
+        currentCount =  parseInt($counter.textContent);
+        if(currentCount % 10 === 0){
+            alert(currentCount+" now");
         }
     }
     for(let index = 0; 
         index < document.getElementsByClassName("js-button").length; index++){
             document.getElementsByClassName("js-button")[index].
             addEventListener("click",(e) => clickHandler(e))
+    }
+    if($counter.textContent == 10){
+        alert("Over 10!");
     }
 })();
