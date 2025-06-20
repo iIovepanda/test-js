41d72b1ef39fe41f323c5bc99ab850fcde571ca5
ayaka-windows <ayakaka0814@gmail.com>
2025-06-20 21:13:38 +0900

    Modified reset behavior

--- reset.js
+++ reset.js
@@ -1,7 +1,9 @@
 (() => {
     const $counter = document.getElementById("js-counter");
+    let $counterBox  = document.querySelector(".counter");
     const clickHandler = () => {
         $counter.textContent = 0;
+        $counterBox.style.backgroundColor = "#FFFFFF";
     }
     document.getElementById("js-reset-button").addEventListener
     ("click",clickHandler);
