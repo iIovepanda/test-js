0024555ae14015dc7688b40cf67b5aa56ef3fb31
ayaka-windows <ayakaka0814@gmail.com>
2025-06-20 20:55:41 +0900

    Added background color change

--- counter.js
+++ counter.js
@@ -9,21 +9,26 @@
         }else if($targetButton.textContent === "+10"){
             $counter.textContent = currentCount + 10; 
         }else if($targetButton.textContent === "-10"){
-            $counter.textContent = currentCount - 10; s
+            $counter.textContent = currentCount - 10; 
         }else{
             $counter.textContent = currentCount - 1;
         }
         currentCount =  parseInt($counter.textContent);
-        if(currentCount % 10 === 0){
+        let $counterBox  = document.querySelector(".counter");
+        if(currentCount % 10 === 0 && currentCount !== 0){        
             alert(currentCount+" now");
         }
+        if(currentCount === 0){
+            $counterBox.style.backgroundColor = "#FFFFFF";
+        }else if(currentCount > 0){
+            $counterBox.style.backgroundColor = "#FFB6C1";
+        }else{
+            $counterBox.style.backgroundColor = "#ADD8E6";
+        }    
     }
     for(let index = 0; 
         index < document.getElementsByClassName("js-button").length; index++){
             document.getElementsByClassName("js-button")[index].
             addEventListener("click",(e) => clickHandler(e))
-    }
-    if($counter.textContent == 10){
-        alert("Over 10!");
     }
 })();
